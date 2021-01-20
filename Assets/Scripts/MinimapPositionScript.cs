b8cf623d18e39b146bcd1f08c9e83fd8591dd9e8
Sahil Mahendrakar <sahilmahendrakar@Sahils-MacBook-Pro.local>
2021-01-19 20:04:51 -0500

    initial commit

---
+++ Assets/Scripts/MinimapPositionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapPositionScript : MonoBehaviour
+{
+
+    public Camera camera;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = camera.transform.position + offset;
+    }
+}
